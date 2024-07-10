39082b6c366416295a546adbb46e030dceb8346b
no.more.caffeine <xuanquoc1352002@gmail.com>
2024-07-10 12:16:54 +0700

    update

--- elliptic_curve/src/lib.rs
+++ elliptic_curve/src/lib.rs
@@ -1,8 +1,7 @@
 use std::ops::Add;
 use finite_field::FiniteField as FF;
 use num_bigint::BigUint;
-// use num_traits::{FromPrimitive, One, Zero};
-
+use num_traits::{Num, Zero};
 
 #[derive(Debug, Clone)]
 pub struct Point {
@@ -56,7 +55,6 @@ impl PartialEq for Point {
         && self.b == other.b
     }    
 }
-
 
 impl Add for Point {
     type Output = Self;
@@ -122,33 +120,66 @@ impl Add for Point {
         }
 }
 
+pub struct Secp256k1 {
+    pub a: FF,
+    pub b: FF,
+    pub p: BigUint,
+    pub g: Point,
+    pub n: BigUint,
+}
 
+impl Secp256k1 {
+    pub fn new() -> Self {
+        let p = BigUint::from_str_radix("fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f", 16).unwrap();
+        let a = FF::new(BigUint::zero(), p.clone());
+        let b = FF::new(BigUint::from(7_u32), p.clone());
+
+        let x = BigUint::from_str_radix("79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798", 16).unwrap();
+        let y = BigUint::from_str_radix("483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8", 16).unwrap();
+        let x = FF::new(x, p.clone());
+        let y = FF::new(y, p.clone());
+        let g = Point::new(Some(x), Some(y), a.clone(), b.clone());
+
+        let n = BigUint::from_str_radix("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16).unwrap();
+        Self {a, b, p, g, n}
+    }
+
+    pub fn g(&self) -> &Point {
+        &self.g
+    }
+
+    pub fn n(&self) -> &BigUint {
+        &self.n
+    }
+
+    pub fn point(&self, x: BigUint, y: BigUint) -> Point {
+        Point::new(Some(FF::new(x, self.p.clone())), Some(FF::new(y, self.p.clone())), self.a.clone(), self.b.clone())
+    }
+}
 
 
 #[cfg(test)]
 mod tests {
     use std::str::FromStr;
-
     use num_bigint::BigUint;
     use num_traits::{FromPrimitive, Num, Zero};
     use super::*;
 
     #[test]
-    fn test_secp256k1() {
+    fn test_create_point() {
         let x = "8837213008c5d8a75b57fa57caf34709bd5ce952e803a14f801c4d0cd3e7a688";
         let y = "16a99921859b01771522bd63d463508217b9e0b3cd2d85a657202e644f55e5f3";
     
         let x = BigUint::from_str_radix(x, 16).unwrap();
         let y = BigUint::from_str_radix(y, 16).unwrap();
     
-        let p = BigUint::from(2_u32).pow(256_u32)
-            - BigUint::from(2_u32).pow(32_u32)
-            - BigUint::from(977_u32);
-        
-        let gx = FF::new(x, p.clone());
-        let gy = FF::new(y, p.clone());
+        let p = BigUint::from_str_radix("fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f", 16).unwrap();
         let a = FF::new(BigUint::zero(), p.clone());
         let b = FF::new(BigUint::from(7_u32), p.clone());
+
+        let gx = FF::new(x, p.clone());
+        let gy = FF::new(y, p.clone());
+        
 
         let k = Point::new(Some(gx.clone()),Some(gy.clone()),a.clone(),b.clone());
         let k1 = Point::new(Some(gx.clone()),Some(gy.clone()),a.clone(),b.clone());
@@ -216,6 +247,16 @@ mod tests {
         let rs = Point::new(Some(x.clone()), Some(y.clone()), a.clone(), b.clone());
 
         assert_eq!(pp.scalar_mul(n), rs);
+    }
+
+    #[test]
+    pub fn test_secp256k1() {
+        let secp256k1 = Secp256k1::new();
+        let x = BigUint::from_str_radix("79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798", 16).unwrap();
+        let y = BigUint::from_str_radix("483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8", 16).unwrap();
+        let g = secp256k1.point(x, y);
+        assert_eq!(secp256k1.g(), &g);
+        assert_eq!(secp256k1.g.scalar_mul(100) + g.clone(), g.clone().scalar_mul(101));
     }
     
 }
