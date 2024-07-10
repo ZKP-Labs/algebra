19cf960a9754f7542e41d2efb0d1dbdee481c849
no.more.caffeine <xuanquoc1352002@gmail.com>
2024-07-10 13:47:22 +0700

    update

--- elliptic_curve/src/point.rs
+++ elliptic_curve/src/point.rs
@@ -153,9 +153,7 @@ mod tests {
 
     #[test]
     fn test_addition() {
-        let p = BigUint::from_u8(2).unwrap().pow(256_u32)
-            - BigUint::from_u8(2).unwrap().pow(32_u32)
-            - BigUint::from_u32(977).unwrap();
+        let p = BigUint::from_str_radix("fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f", 16).unwrap();
         let a = FF::new(BigUint::zero(), p.clone());
         let b = FF::new(BigUint::from_u32(7).unwrap(), p.clone());
 
@@ -188,9 +186,8 @@ mod tests {
 
     #[test]
     pub fn test_scalar() {
-        let p = BigUint::from_u8(2).unwrap().pow(256_u32)
-            - BigUint::from_u8(2).unwrap().pow(32_u32)
-            - BigUint::from_u32(977).unwrap();
+        let p = BigUint::from_str_radix("fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f", 16).unwrap();
+
         let a = FF::new(BigUint::zero(), p.clone());
         let b = FF::new(BigUint::from_u32(7).unwrap(), p.clone());
 
@@ -211,15 +208,4 @@ mod tests {
 
         assert_eq!(pp.scalar_mul(n), rs);
     }
-
-    #[test]
-    pub fn test_secp256k1() {
-        let secp256k1 = Secp256k1::new();
-        let x = BigUint::from_str_radix("79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798", 16).unwrap();
-        let y = BigUint::from_str_radix("483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8", 16).unwrap();
-        let g = secp256k1.point(x, y);
-        assert_eq!(secp256k1.g(), &g);
-        assert_eq!(secp256k1.g.scalar_mul(100) + g.clone(), g.clone().scalar_mul(101));
-    }
-    
 }
